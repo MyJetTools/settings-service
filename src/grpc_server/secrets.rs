a14382acefb02e12a8ab99c4925fff856fb2bd62
amigin <amigin@gmail.com>
2023-08-30 17:04:18 +0300

    GetAllSecrets

--- src/grpc_server/secrets.rs
+++ src/grpc_server/secrets.rs
@@ -1,3 +1,7 @@
+use std::pin::Pin;
+
+use futures_core::Stream;
+
 use super::server::GrpcService;
 use crate::app_ctx::SecretsValueReader;
 use crate::secrets_grpc::secrets_server::Secrets;
@@ -5,6 +9,44 @@ use crate::secrets_grpc::*;
 
 #[tonic::async_trait]
 impl Secrets for GrpcService {
+    type GetAllStream =
+        Pin<Box<dyn Stream<Item = Result<SecretListItem, tonic::Status>> + Send + Sync + 'static>>;
+
+    async fn get_all(
+        &self,
+        _: tonic::Request<()>,
+    ) -> Result<tonic::Response<Self::GetAllStream>, tonic::Status> {
+        if let Some(items) = crate::operations::get_all_secrets(&self.app).await {
+            let mut result = Vec::with_capacity(items.len());
+            for item in items {
+                let templates_amount =
+                    crate::operations::secrets::get_used_secret_amount_by_template(
+                        &self.app,
+                        item.get_secret_name(),
+                    )
+                    .await;
+
+                let secrets_amount = crate::operations::secrets::get_used_secret_amount_by_secret(
+                    &self.app,
+                    item.get_secret_name(),
+                )
+                .await;
+
+                result.push(SecretListItem {
+                    name: item.row_key.clone(),
+                    level: item.level.unwrap_or(0) as i32,
+                    created: item.create_date.clone(),
+                    updated: item.last_update_date.clone(),
+                    used_by_secrets: secrets_amount as i32,
+                    used_by_templates: templates_amount as i32,
+                });
+            }
+            my_grpc_extensions::grpc_server::send_vec_to_stream(result, |item| item).await
+        } else {
+            my_grpc_extensions::grpc_server::send_vec_to_stream(vec![], |item| item).await
+        }
+    }
+
     async fn get(
         &self,
         request: tonic::Request<GetSecretRequest>,
