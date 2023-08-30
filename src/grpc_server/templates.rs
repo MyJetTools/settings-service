3f32454ad5f4c96733397ef41c46dcfe0fdf80bd
amigin <amigin@gmail.com>
2023-08-30 16:40:23 +0300

    All templates now can be read by GRPC

--- src/grpc_server/templates.rs
+++ src/grpc_server/templates.rs
@@ -1,9 +1,46 @@
+use std::pin::Pin;
+
+use futures_core::Stream;
+
 use super::server::GrpcService;
 use crate::templates_grpc::templates_server::Templates;
 use crate::templates_grpc::*;
 
 #[tonic::async_trait]
 impl Templates for GrpcService {
+    type GetAllStream = Pin<
+        Box<dyn Stream<Item = Result<TemplateListItem, tonic::Status>> + Send + Sync + 'static>,
+    >;
+
+    async fn get_all(
+        &self,
+        _: tonic::Request<()>,
+    ) -> Result<tonic::Response<Self::GetAllStream>, tonic::Status> {
+        let result = crate::operations::get_all_templates(&self.app).await;
+        let time_snapshot = self.app.last_request.get_snapshot().await;
+
+        my_grpc_extensions::grpc_server::send_vec_to_stream(result, move |item| {
+            let last_time = if let Some(sub_items) = time_snapshot.get(&item.partition_key) {
+                if let Some(value) = sub_items.get(&item.row_key) {
+                    value.unix_microseconds / 1000
+                } else {
+                    0
+                }
+            } else {
+                0
+            };
+
+            TemplateListItem {
+                env: item.partition_key.clone(),
+                name: item.row_key.clone(),
+                created: item.create_date.clone(),
+                updated: item.last_update_date.clone(),
+                last_requests: last_time,
+            }
+        })
+        .await
+    }
+
     async fn get(
         &self,
         request: tonic::Request<GetTemplateRequest>,
