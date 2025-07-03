ed9a59de94b0ef5489f4edae359d14e1153cf889
amigin <amigin@gmail.com>
2025-07-03 18:03:56 +0300

    Debugging

--- src/flows/templates/get_populated.rs
+++ src/flows/templates/get_populated.rs
@@ -1,7 +1,8 @@
 use crate::app_ctx::AppContext;
 
-pub async fn get_populated_template(app: &AppContext, evn: &str, name: &str) -> Option<String> {
-    let template = super::get(app, evn, name).await?;
+pub async fn get_populated_template(app: &AppContext, env: &str, name: &str) -> Option<String> {
+    println!("Getting populated template {}/{}", env, name);
+    let template = super::get(app, env, name).await?;
     crate::scripts::populate_with_secrets(app, template.yaml_template.as_str())
         .await
         .into()
