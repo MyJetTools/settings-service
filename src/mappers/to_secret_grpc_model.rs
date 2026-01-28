a74807d0770ebb176b32ae5122bf920450b7ac1d
amigin <amigin@gmail.com>
2026-01-28 16:24:46 +0200

    Debugging

--- src/mappers/to_secret_grpc_model.rs
+++ src/mappers/to_secret_grpc_model.rs
@@ -4,22 +4,22 @@ pub async fn to_secret_grpc_model(
     app: &AppContext,
     product_id: ProductId<'_>,
     secrets: &SecretsSnapshot,
-    item: &SecretItem,
+    secret_item: &SecretItem,
 ) -> SecretGrpcModel {
     let used_by_secrets = secrets.get_count(product_id, |itm| {
-        itm.content.has_the_secret_inside(&item.id)
+        itm.content.has_the_secret_inside(&secret_item.id)
     });
 
     let used_by_templates = app
         .templates
-        .get_count_from_all(|itm| itm.content.has_secret_inside())
+        .get_count_from_all(|itm| itm.content.has_the_secret_inside(&secret_item.id))
         .await;
 
     SecretGrpcModel {
-        secret_id: item.id.to_string(),
-        level: item.level as i32,
-        created: item.created.to_rfc3339(),
-        updated: item.updated.to_rfc3339(),
+        secret_id: secret_item.id.to_string(),
+        level: secret_item.level as i32,
+        created: secret_item.created.to_rfc3339(),
+        updated: secret_item.updated.to_rfc3339(),
         used_by_secrets: used_by_secrets as i32,
         used_by_templates: used_by_templates as i32,
         product_id: match product_id {
